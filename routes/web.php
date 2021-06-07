929edbe6dcf7cf3679370e76aa8740e68dafc248
kekeli543 <kekeli543@gmail.com>
2021-06-07 22:05:04 +0800

    修改路由表

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\PostController;
+Route::resource('posts', 'PostController::class');
+//Route::resource('posts', 'App\Http\Controllers\PostController');
